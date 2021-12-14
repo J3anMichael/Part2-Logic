1ab3ccc8a9b47f8d156fcf005e8d064c4ad9ea68
Jean Michael <jeanmmclaudino@gmail.com>
2021-12-13 22:41:01 -0300

    Exercicio estrura repeticao

--- Part2 Logic/Program.cs
+++ Part2 Logic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Part2
 {
@@ -24,34 +25,76 @@ namespace Part2
 
             // SINTAXE DE FUNCOES
 
-            Console.WriteLine("Digite tres numeros: ");
-            int n1 = int.Parse(Console.ReadLine());
-            int n2 = int.Parse(Console.ReadLine());
-            int n3 = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("Digite tres numeros: ");
+            //    int n1 = int.Parse(Console.ReadLine());
+            //    int n2 = int.Parse(Console.ReadLine());
+            //    int n3 = int.Parse(Console.ReadLine());
 
 
-            double resultado = Maior(n1, n2, n3);
+            //    double resultado = Maior(n1, n2, n3);
 
-            Console.WriteLine("Maior = " + resultado);
-        }
+            //    Console.WriteLine("Maior = " + resultado);
+            //}
 
 
-        static int Maior(int a, int b, int c)
-        {
-            int m;
-            if (a > b && a > c)
+            //static int Maior(int a, int b, int c)
+            //{
+            //    int m;
+            //    if (a > b && a > c)
+            //    {
+            //        m = a;
+            //    }
+            //    else if (b > c)
+            //    {
+            //        m = b;
+
+            //    } else
+            //    {
+            //        m = c;
+            //    }
+            //    return m;
+
+
+            // ESTRUTURA DE REPETICAO 
+
+            // SITAXY
+
+            //while (  CONDICAO )
+            //{
+            //    COMANDO1
+            //        COMANDO2
+            //}
+
+
+
+            //Console.Write("Type a number: ");
+            //double x = double.Parse(Console.ReadLine());
+
+            //while (x >= 0.0)
+            //{
+            //    double raiz = Math.Sqrt(x);
+            //    Console.Write(raiz.ToString("F3"));
+            //    Console.Write("Type another number: ");
+            //    x = double.Parse(Console.ReadLine());
+
+            //    Console.Write("Negative number bye!");
+
+            // EXERCICIO DE FIXACAO
+
+
+            Console.Write("Type your password: ");
+            int password = int.Parse(Console.ReadLine());
+
+            while ( password != 2002)
             {
-                m = a;
+                Console.Write("Wrong password, try again!");
+
+                password = int.Parse(Console.ReadLine());
+
+                
             }
-            else if (b > c)
-            {
-                m = b;
+            Console.WriteLine("Welcome Jean Michael");
 
-            } else
-            {
-                m = c;
-            }
-            return m;
         }
 
     }
